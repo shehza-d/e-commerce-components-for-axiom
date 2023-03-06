198632ed96b820f6d42cf1558114a850bb2c34f9
Shehzad <shehzaddiqbal@gmail.com>
2023-03-06 21:59:56 +0500

    push

--- src/components/ImgGird1/index.tsx
+++ src/components/ImgGird1/index.tsx
@@ -6,14 +6,14 @@ export default function ImgGrid1(props: { data: ImgGridType[] }) {
     <>
       <section className="m-7 min-h-screen">
         <div className="flex flex-wrap">
-          <Img data={props.data[0]} />
-          <Img data={props.data[1]} />
-          <Img data={props.data[2]} />
+          <Img data={props?.data[0]} />
+          <Img data={props?.data[1]} />
+          <Img data={props?.data[2]} />
         </div>
         <div className="flex flex-wrap">
-          <Img data={props.data[3]} />
-          <Img data={props.data[4]} />
-          <Img data={props.data[5]} />
+          <Img data={props?.data[3]} />
+          <Img data={props?.data[4]} />
+          <Img data={props?.data[5]} />
         </div>
       </section>
     </>
